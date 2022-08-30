87753cba356d766f6fbdd0ccdeea8969c2e45e3a
yichui <896873144@qq.com>
2022-08-31 04:34:09 +0800

    change param

--- Assets/NPR/Genshin/RadRotate.cs
+++ Assets/NPR/Genshin/RadRotate.cs
@@ -7,14 +7,6 @@ public class RadRotate : MonoBehaviour
     public float x = 0f;
     public float y = 0f;
     public float z = 0f;
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-
 
     void Update()
     {
